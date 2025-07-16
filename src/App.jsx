307a9923442bbbb6e620bb5df905d4596511fe6a
Dima <dmitr.burii.2016@gmail.com>
2025-07-16 21:15:44 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-
 import Home from './pages/Home/Home.jsx';
 import Lifts from './pages/Lifts/Lifts.jsx';
 import Escalators from './pages/Escalators/Escalators.jsx';
@@ -9,9 +8,11 @@ import Parking from './pages/Parking/Parking.jsx';
 import OtherProducts from './pages/OtherProducts/OtherProducts.jsx';
 import Contacts from './pages/Contacts/Contacts.jsx';
 
+const basename = import.meta.env.MODE === 'production' ? '/elevador' : '/';
+
 function App() {
   return (
-    <Router>
+    <Router basename={basename}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './header.css'
 import { NavLink } from 'react-router-dom'
+import logo from '/logo.svg'
 export default function Header() {
   return (
     <header className='header'>
       <div className="container">
         <div className="header__inner">
           <NavLink to='/' className='header__logo'>
-            <img src="logo.svg" alt="elevador" />
+            <img src={logo} alt="elevador" />
           </NavLink>
           <nav className='header__nav'>
             <button className='header__nav-item'>
