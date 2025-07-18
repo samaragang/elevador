de1c48a8850f90a4a52ec647e6a65bffaee2d48d
Dima <dmitr.burii.2016@gmail.com>
2025-07-18 19:48:35 +0300

    change intro-bg

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      svgo: { plugins: [{ name: 'removeViewBox' }] },
+      webp: { quality: 75 },
+    }),],
   base: '/',
 })
